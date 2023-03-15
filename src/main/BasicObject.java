aebd580097a45b2d0dbf471fc71114f6d2bddf1c
yyz <victor@g.ncu.edu.tw>
2023-03-15 21:01:50 +0800

    Merge related class file into same file.

--- src/main/BasicObject.java
+++ src/main/BasicObject.java
@@ -1,9 +1,24 @@
 package main;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class BasicObject extends JLabel {
 	public BasicObject() {
 		
+	}
+}
+
+class ClassObject extends BasicObject {
+	public ClassObject() {
+		this.setSize(64, 64);
+		this.setIcon(new ImageIcon("assets\\class.png"));
+	}
+}
+
+class UCObject extends BasicObject {
+	public UCObject() {
+		this.setSize(64, 64);
+		this.setIcon(new ImageIcon("assets\\uc.png"));
 	}
 }
--- src/main/State.java
+++ src/main/State.java
@@ -5,3 +5,69 @@ import java.awt.event.MouseEvent;
 public interface State {
 	public void onclick(Canvas canvas, MouseEvent e);
 }
+
+class SelectState implements State {
+	public SelectState() {
+		
+	}
+	
+	public void onclick(Canvas canvas, MouseEvent e) {
+		
+	}
+}
+
+class ALState implements State {
+	public ALState() {
+		
+	}
+	
+	public void onclick(Canvas canvas, MouseEvent e) {
+		
+	}
+}
+
+class CLState implements State {
+	public CLState() {
+		
+	}
+	
+	public void onclick(Canvas canvas, MouseEvent e) {
+		
+	}
+}
+
+class ClassState implements State {
+	public ClassState() {
+		
+	}
+	
+	public void onclick(Canvas canvas, MouseEvent e) {
+		ClassObject co = new ClassObject();
+		canvas.add(co);
+		co.setLocation(e.getPoint());
+		canvas.repaint();
+	}
+}
+
+class GLState implements State {
+	public GLState() {
+		
+	}
+	
+	public void onclick(Canvas canvas, MouseEvent e) {
+		
+	}
+}
+
+class UCState implements State {
+	public UCState() {
+		
+	}
+	
+	public void onclick(Canvas canvas, MouseEvent e) {
+		UCObject uco = new UCObject();
+		canvas.add(uco);
+		uco.setLocation(e.getPoint());
+		canvas.repaint();
+	}
+}
